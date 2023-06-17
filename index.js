4e7147a8b9b56a853547c26d925e9120d2a0f6ce
Vany <vany.ikuze@student.moringaschool.com>
2023-06-17 21:44:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const namesGifts =[...names,"surprise"];
+
+function writeCards(names){
+    const newArray = []
+    for(let v = 0; v < names.length; v++){
+        let message = `Thank you, ${names[v]}, for the wonderful surprise gift!`
+         newArray.push(message)
+
+}
+    return newArray;
+}
+console.log(writeCards(names));
+
+function countDown(){
+   let countDown = 0;
+   while (countDown < 11){
+    console.log(countDown++)
+   }
+}
